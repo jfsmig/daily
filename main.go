799316742ce8461bf6c33447654512ea6cf116c4
Jean-Francois Smigielski <jf.smigielski@gmail.com>
2023-03-26 23:16:39 +0200

    Add a sitemap
    It would have been sad to not have it, isn't it?

--- main.go
+++ main.go
@@ -15,10 +15,12 @@ package main
 
 import (
 	_ "embed"
-	"html/template"
+	ht "html/template"
 	"log"
 	"net/http"
 	"strings"
+	tt "text/template"
+	"time"
 
 	"github.com/jfsmig/daily/excuse"
 )
@@ -32,18 +34,45 @@ var robots []byte
 //go:embed index.html
 var templateIndexText string
 
+//go:embed sitemap.xml
+var templateSitemapText string
+
 type HandlerFunc func(http.ResponseWriter, *http.Request)
 
 func main() {
+
+	http.HandleFunc("/sitemap.xml", func() HandlerFunc {
+		type Args struct {
+			Date string
+		}
+		tpl := tt.Must(tt.New("sitemap").Parse(templateSitemapText))
+		args := Args{Date: time.Now().Truncate(25 * time.Hour).Format(time.RFC3339)}
+		return func(w http.ResponseWriter, req *http.Request) {
+			w.Header().Add("Content-Type", "application/xml")
+			w.Header().Set("Cache-Control", "public, max-age=86400") // 1 day
+			if err := tpl.Execute(w, args); err != nil {
+				log.Println("Sitemap rendering error:", err)
+			}
+		}
+	}())
+
 	http.HandleFunc("/favicon.png", func() HandlerFunc {
 		return func(w http.ResponseWriter, req *http.Request) {
-			w.Write(icon)
+			w.Header().Add("Content-Type", "image/png")
+			w.Header().Set("Cache-Control", "public, max-age=604800") // 1 week
+			if _, err := w.Write(icon); err != nil {
+				log.Println("Icon reply error:", err)
+			}
 		}
 	}())
 
 	http.HandleFunc("/robots.txt", func() HandlerFunc {
 		return func(w http.ResponseWriter, req *http.Request) {
-			w.Write(robots)
+			w.Header().Add("Content-Type", "text/plain")
+			w.Header().Set("Cache-Control", "public, max-age=604800") // 1 week
+			if _, err := w.Write(robots); err != nil {
+				log.Println("Robots reply error:", err)
+			}
 		}
 	}())
 
@@ -55,15 +84,17 @@ func main() {
 		if err != nil {
 			log.Fatalln("excuse init error: ", err)
 		}
-		tpl := template.Must(template.New("index").Parse(templateIndexText))
+		tpl := ht.Must(ht.New("index").Parse(templateIndexText))
 		return func(w http.ResponseWriter, req *http.Request) {
 			env := excuse.NewEnv()
 			var sb strings.Builder
 			_ = nodaily.Expand(req.Context(), &sb, env)
+			w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+			w.Header().Set("Pragma", "no-cache")
+			w.Header().Set("Expires", "0")
 			args := Args{Excuse: sb.String()}
 			if err := tpl.Execute(w, args); err != nil {
 				log.Println("Template rendering error:", err)
-				w.WriteHeader(500)
 			}
 		}
 	}())
